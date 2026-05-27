9d30f82aeaecbe668eeb37b4f49a9ceb9756c0af
alexandredinero17 <alexandredinero17@gmail.com>
2026-05-27 20:56:14 +0000

    fix bcrypt render

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const fs = require('fs');
 const path = require('path');
 const mysql = require('mysql2');
