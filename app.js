03cec65883340b31947da196a679ac3d796ce086
alexandredinero17 <alexandredinero17@gmail.com>
2026-05-29 05:55:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 30
     }
 }));
+
+
 async function getOrCreateConversation(user1, user2) {
 
     const existing = await db.query(
@@ -85,6 +87,56 @@ async function getOrCreateConversation(user1, user2) {
 
     return created.rows[0].id;
 }
+const storage = multer.diskStorage({
+
+destination: (req, file, cb) => {
+
+    const dir = path.join(__dirname, 'public/uploads');
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    cb(null, dir);
+},
+
+filename: (req, file, cb) => {
+
+    const unique =
+        Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    cb(
+        null,
+        unique + path.extname(file.originalname)
+    );
+}
+});
+
+const upload = multer({
+
+storage,
+
+limits: {
+    fileSize: 5 * 1024 * 1024
+},
+
+fileFilter: (req, file, cb) => {
+
+    const allowed = [
+        "image/jpeg",
+        "image/png",
+        "image/jpg",
+        "image/webp"
+    ];
+
+    if (allowed.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error("Format image invalide"));
+    }
+}   
+});
+
 
 // ================= ADMIN =================
 function isAdmin(req, res, next) {
